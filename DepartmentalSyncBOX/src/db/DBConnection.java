6b618a6be5c7d618d0d2e8124d0457bbb1c76f45
Ebed meleck <ituzeebedi12@gmail.com>
2025-11-08 21:39:19 +0200

    refactor:added coonnection to database jdbc my sql driver

--- DepartmentalSyncBOX/src/db/DBConnection.java
+++ DepartmentalSyncBOX/src/db/DBConnection.java
@@ -1,28 +1,60 @@
 package db;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.util.Properties;
-import java.io.InputStream;
-
+import java.sql.*;
 public class DBConnection {
-    private static Connection connection;
+    private static Connection connection = null;
+     public  static  final String URL = "jdbc:mysql://localhost:3306/dept_sync_db";
+     public static  final  String USER = "root";
+     public  static final String PASSWORD = "";
+
+
+    // private constructor to prevent instantiation
+    private   DBConnection (){}
+
 
     public static Connection getConnection() {
-        if (connection == null) {
-            try (InputStream input = DBConnection.class.getResourceAsStream("/config/db.properties")) {
-                Properties props = new Properties();
-                props.load(input);
-                String url = props.getProperty("db.url");
-                String user = props.getProperty("db.user");
-                String password = props.getProperty("db.password");
+        try {
+            if(connection == null || connection.isClosed()){
+                // loading driver
 
-                connection = DriverManager.getConnection(url, user, password);
-            } catch (Exception e) {
-                e.printStackTrace();
+                Class.forName("com.mysql.cj.jdbc.Driver");
+
+                // Establish connection
+
+                connection = DriverManager.getConnection(URL, USER , PASSWORD);
+
+                System.out.println("Connection successfull ");
+
+
             }
+
+        }catch (ClassNotFoundException | SQLException e){
+             // print out error
+            System.err.println("Connection Failed"+ e.getMessage());
         }
+
+
         return connection;
+
+    }
+
+    // method to close connection
+    public static void  closeConnection(){
+        try {
+            if(connection != null && !connection.isClosed()){
+                connection.close();
+                System.out.println("Database connection closed");
+
+            }
+
+        } catch (Exception e) {
+            System.err.println("Error on closing connection ");
+        }
+    }
+
+
+
+
+    public static void main(String[] args) throws Exception {
+        getConnection();
     }
 }
